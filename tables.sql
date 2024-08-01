51210b140506ff5953565995bf3c717e65a0dbd9
Lukas B <lukas_bernegger@gmx.at>
2024-08-01 13:25:53 +0200

    Select möglichkeiten hinzugefügt.

--- tables.sql
+++ tables.sql
@@ -1,5 +1,9 @@
+-- Tabelle löschen
+DROP TABLE users;
+
 -- Tabelle anlegen
 CREATE TABLE users(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
     username TEXT NOT NULL,
     password TEXT NOT NULL,
     email TEXT NOT NULL
@@ -9,4 +13,26 @@ CREATE TABLE users(
 INSERT INTO
     users (username, password, email)
 VALUES
-    ("Lukas", "geheim", "lukas_bernegger@gmx.at")
+    ("Max", "öleiaf", "max@gmx.at"),
+    ("Lukas", "#ikeldlisef", "lukas@gmx.at"),
+    ("Marin", "ääliesseed", "martin@gmx.at")
+
+
+-- Daten auslesen/selektieren (Anzeigen nur username und passwort)
+SELECT username, password FROM users;  
+
+-- Alle Daten auslesen/selektieren (Alle Spalten anzeigen mit *)
+SELECT * FROM users;
+
+-- Bestimmte Spalte suchen.
+SELECT * FROM users WHERE username = "Lukas";
+
+-- Alle Daten auslesen/selektieren nach id
+SELECT * FROM users WHERE id = 2;
+
+-- Alle Daten auslesen/selektieren uns aufsteigend sortieren.
+SELECT * FROM users ORDER BY username ASC;
+
+-- Alle Daten auslesen/selektieren uns absteigend sortieren.
+SELECT * FROM users ORDER BY username DESC;
+
